9a0ce0bc7abc9f60c5633429d78e3a4b8a4a8a94
jbower493 <60370886+jbower493@users.noreply.github.com>
2024-04-01 19:07:19 +1100

    fix: dont check for logged in user to own quantity units

--- app/Http/Controllers/QuantityUnitsController.php
+++ app/Http/Controllers/QuantityUnitsController.php
@@ -3,15 +3,12 @@
 namespace App\Http\Controllers;
 
 use App\Models\QuantityUnit;
-use Illuminate\Support\Facades\Auth;
 
 class QuantityUnitsController extends Controller
 {
     public function index()
     {
-        $loggedInUserId = Auth::id();
-
-        $quantityUnits = QuantityUnit::where('user_id', $loggedInUserId)->orderBy('name')->get()->toArray();
+        $quantityUnits = QuantityUnit::all()->orderBy('name')->get()->toArray();
 
         return [
             'message' => 'Successfully retreived quantity units.',
